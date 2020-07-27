11b9389ec81fd6ae8e690b58112a6a3097bfd785
ghuanz <kong@mail.com>
2020-07-27 15:18:02 +0700

    up

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,24 +1,24 @@
 @extends('layouts.content')
 @section('content')
-<div class="d-flex flex-row-fluid bgi-size-cover bgi-position-center" style="background-image: url('assets/media/bg/bg-9.jpg')">
-    <div class=" container ">
-        <!--begin::Topbar-->
-        <div class="d-flex justify-content-between align-items-center border-bottom border-white py-7">
-            <h3 class="h4 text-dark mb-0">
-                Home Page
-            </h3>
-            <div class="d-flex">
-                
+    <div class="d-flex flex-row-fluid bgi-size-cover bgi-position-center" style="background-image: url('assets/media/bg/bg-9.jpg')">
+        <div class=" container ">
+            <!--begin::Topbar-->
+            <div class="d-flex justify-content-between align-items-center border-bottom border-white py-7">
+                <h3 class="h4 text-dark mb-0">
+                    Home Page
+                </h3>
+                <div class="d-flex">
+                    
+                </div>
+            </div>
+            <!--end::Topbar-->
+            <div class="d-flex align-items-stretch text-center flex-column py-40">
+                <!--begin::Heading-->
+                <h1 class="text-dark font-weight-bolder mb-12">
+                    Welcome to KKP Aplication
+                </h1>
+                <!--end::Heading-->
             </div>
         </div>
-        <!--end::Topbar-->
-        <div class="d-flex align-items-stretch text-center flex-column py-40">
-            <!--begin::Heading-->
-            <h1 class="text-dark font-weight-bolder mb-12">
-                Welcome to KPP Aplication
-            </h1>
-            <!--end::Heading-->
-        </div>
     </div>
-</div>
 @endsection
